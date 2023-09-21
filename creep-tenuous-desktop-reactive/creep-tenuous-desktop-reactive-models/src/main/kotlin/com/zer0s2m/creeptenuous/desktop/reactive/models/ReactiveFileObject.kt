ee74622adcfcedfe8539e9f250dfb8dee5ece954
Aleksandr <ZeroDay9898@gmail.com>
2023-09-21 22:08:32 +0300

    Deleting a file object

--- creep-tenuous-desktop-reactive/creep-tenuous-desktop-reactive-models/src/main/kotlin/com/zer0s2m/creeptenuous/desktop/reactive/models/ReactiveFileObject.kt
+++ creep-tenuous-desktop-reactive/creep-tenuous-desktop-reactive-models/src/main/kotlin/com/zer0s2m/creeptenuous/desktop/reactive/models/ReactiveFileObject.kt
@@ -4,7 +4,9 @@ import com.zer0s2m.creeptenuous.desktop.common.dto.ManagerFileObject
 import com.zer0s2m.creeptenuous.desktop.core.injection.ReactiveInjection
 import com.zer0s2m.creeptenuous.desktop.core.reactive.Reactive
 import com.zer0s2m.creeptenuous.desktop.core.reactive.ReactiveLazyObject
+import com.zer0s2m.creeptenuous.desktop.core.reactive.ReactiveTrigger
 import com.zer0s2m.creeptenuous.desktop.reactive.handlers.HandlerReactiveFileObjectManagerFileSystemObjects
+import com.zer0s2m.creeptenuous.desktop.reactive.triggers.ReactiveTriggerReactiveFileObjectDeleteFileObject
 
 /**
  * Reactive file object data model
@@ -18,7 +20,13 @@ object ReactiveFileObject : ReactiveLazyObject {
         handler = HandlerReactiveFileObjectManagerFileSystemObjects::class,
         injection = ReactiveInjection(
             method = "setManagerFileObject"
-        )
+        ),
+        triggers = [
+            ReactiveTrigger(
+                event = "deleteFileObject",
+                trigger = ReactiveTriggerReactiveFileObjectDeleteFileObject::class
+            )
+        ]
     )
     var managerFileSystemObjects: ManagerFileObject = ManagerFileObject(
         systemParents = listOf(),
